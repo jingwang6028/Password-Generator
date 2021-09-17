6a84c7beb79692a4bcbf51c83e426857ae083a50
Jing <jingwang6028@gmail.com>
2021-09-17 18:38:34 -0400

    update files and images

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function generatePassword() {
-  console.log('hello world');
 
   // 1 prompt the user for the password criteria
   //   password length 8 ~ 128
